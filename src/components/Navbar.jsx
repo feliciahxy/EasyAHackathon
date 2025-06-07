291f8583624a833390312a3c5bd72347a0a53073
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-06-07 16:31:15 +0800

    fixed bug on menu screen on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,18 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(true);
     const handleClick = () => {
         setToggle(!toggle);
-        console.log("Toggle state:", !toggle);
+        console.log("Toggle state:", toggle);
     }
 
     return (
         <div className="navbar w-full left-0 right-0 top-0 bg-base-100 shadow-sm fixed">
             <div className="flex-none">
-                <div onClick={handleClick}>
-                    <img src={!toggle?menu:close} alt='menu' className='w-[30px] h-[30px]' />
+                <div className="dropdown" onClick={handleClick}>
+                    <img tabIndex={0} role="button" src={toggle ? menu : close} alt='menu' className='w-[30px] h-[30px]'/>
+                    <ul className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                    </ul>
                 </div>
             </div>
             <div className="flex-1">
