790e61c791a7afce63bd0c8eb702423056299a11
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-06-07 17:47:04 +0800

    refined organisations list page

--- src/pages/OrganisationList.jsx
+++ src/pages/OrganisationList.jsx
@@ -1,40 +1,50 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function OrganisationList() {
     return (
         <div className="flex items-center justify-center w-screen gap-x-8">
-            <div className="card w-96 bg-base-100 card-xl shadow-lg">
-                <div className="card-body">
-                    <h2 className="card-title">Organisations</h2>
-                    <p>Placeholder Subtext</p>
-                    <ul>
-                        <li>Organisation 1</li>
-                        <li>Organisation 2</li>
-                        <li>Organisation 3</li>
-                    </ul>
+            <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+                <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">Organisation Name</h2>
+                <p>Organisation Description</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary"><Link to="/escrow">Donate Now</Link></button>
                 </div>
             </div>
-            <div className="card w-96 bg-base-100 card-xl shadow-lg">
-                <div className="card-body">
-                    <h2 className="card-title">Organisations</h2>
-                    <p>Placeholder Subtext</p>
-                    <ul>
-                        <li>Organisation 1</li>
-                        <li>Organisation 2</li>
-                        <li>Organisation 3</li>
-                    </ul>
+            </div>
+            <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+                <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">Organisation Name</h2>
+                <p>Organisation Description</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary"><Link to="/escrow">Donate Now</Link></button>
                 </div>
             </div>
-            <div className="card w-96 bg-base-100 card-xl shadow-lg">
-                <div className="card-body">
-                    <h2 className="card-title">Organisations</h2>
-                    <p>Placeholder Subtext</p>
-                    <ul>
-                        <li>Organisation 1</li>
-                        <li>Organisation 2</li>
-                        <li>Organisation 3</li>
-                    </ul>
+            </div>
+            <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+                <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">Organisation Name</h2>
+                <p>Organisation Description</p>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary"><Link to="/escrow">Donate Now</Link></button>
                 </div>
+            </div>
             </div>
         </div>
     );
