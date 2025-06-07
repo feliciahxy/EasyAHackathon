99c2c97c86ee832b96474d95ecd50c2be88649c0
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-06-07 17:00:19 +0800

    refined home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     }
 
     return (
-        <div className="navbar w-full left-0 right-0 top-0 bg-base-100 shadow-sm fixed">
+        <div className="navbar w-full left-0 right-0 top-0 bg-base-100 shadow-sm fixed z-50">
             <div className="flex-none">
                 <div className="dropdown" onClick={handleClick}>
                     <img tabIndex={0} role="button" src={toggle ? menu : close} alt='menu' className='w-[30px] h-[30px]'/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
 
 import React, {useState} from 'react';
-import reactLogo from '../assets/react.svg';
-import viteLogo from '../assets/vite.svg';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 const Home = () => {
     const [count, setCount] = useState(0);
     return (
         <div
-        className="hero min-h-screen"
+        className="hero min-h-screen w-screen relative"
         style={{
             backgroundImage:
             "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
@@ -17,12 +16,13 @@ const Home = () => {
         <div className="hero-overlay"></div>
         <div className="hero-content text-neutral-content text-center">
             <div className="max-w">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+            <h1 className="mb-5 text-5xl font-bold">SAMPLE NAME</h1>
+            <h1 className="mb-5 text-l">Crowdfunding made seamless and transparent </h1>
+            <div className="flex flex-row justify-center items-center gap-4 mt-6">
+                <button className="btn btn-primary text-neutral-content text-base md:text-lg px-6 py-2"><Link to="/create-wallet">Create Wallet</Link></button>
+                <button className="btn btn-primary text-neutral-content text-base md:text-lg px-6 py-2"><Link to="/escrow">Send Currency</Link></button>
+            </div>
+            
             </div>
         </div>
         </div>
