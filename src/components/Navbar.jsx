8f65400c5516ee9902d9a3624bae20dea262c835
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-06-07 16:42:35 +0800

    edited home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,19 @@ const Navbar = () => {
         console.log("Toggle state:", toggle);
     }
 
+    const redirectPage = () => {
+        setToggle(true);
+        console.log("Toggle state:", toggle);
+    }
+
     return (
         <div className="navbar w-full left-0 right-0 top-0 bg-base-100 shadow-sm fixed">
             <div className="flex-none">
                 <div className="dropdown" onClick={handleClick}>
                     <img tabIndex={0} role="button" src={toggle ? menu : close} alt='menu' className='w-[30px] h-[30px]'/>
                     <ul className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                        <li><a><Link to="/create-wallet">Create Wallet</Link></a></li>
-                        <li><a><Link to="/escrow">Send Currency</Link></a></li>
+                        <li onClick={redirectPage}><a><Link to="/create-wallet">Create Wallet</Link></a></li>
+                        <li onClick={redirectPage}><a><Link to="/escrow">Send Currency</Link></a></li>
                     </ul>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,28 +7,25 @@ import '../App.css';
 const Home = () => {
     const [count, setCount] = useState(0);
     return (
-        <div>
-            {/* <div className = "flex justify-center">
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1 className = "flex justify-center">Vite + React</h1> */}
-            <div className="card">
-                <button className = "btn flex justify-center" onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-                </button>
-                <p>
-                Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
+        <div
+        className="hero min-h-screen"
+        style={{
+            backgroundImage:
+            "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+        }}
+        >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+            <div className="max-w">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.
             </p>
-      </div>
+            <button className="btn btn-primary">Get Started</button>
+            </div>
+        </div>
+        </div>
     );
 };
 export default Home;
