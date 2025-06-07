f486bbaa15cf132f9979374e62dc3f4d07d835b5
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-06-07 13:37:37 +0800

    added menu icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import { menu } from "../assets";
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(true);
@@ -9,10 +10,10 @@ const Navbar = () => {
     }
 
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar w-full h-[80px] bg-base-100 shadow-sm">
         <div className="flex-none">
             <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+                <Link to="/"><img src={menu} alt='menu' className='w-[50px] h-[50px]' /></Link>
             </button>
         </div>
         <div className="flex-1">
